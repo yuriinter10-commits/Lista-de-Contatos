2ebcac0d2389a1c7f90ce66549ae694fb2b0cbaa
YURI DE OLIVEIRA RODRIGUES <87007240097@senacrs.edu.br>
2026-04-06 21:22:07 -0300

    lista de contato

---
+++ Desafio-Agenda_de_celular.py
@@ -0,0 +1,58 @@
+import time
+import os
+
+agenda = []
+TEMPO = 5
+  
+while True:
+    os.system("cls")
+    print("======AGENDA======")
+    print("1- Adicionar: ")
+    print("2- Ver Contato: ")
+    print("3- SAIR: ")
+
+    opcao = int(input("Digite uma opcao:    "))
+
+    if opcao == 1:
+        os.system("cls")
+        nome=str(input("Digite nome :"))
+        numero=int(input("Digite numero :"))
+    
+        contato = {
+        "nome":nome,
+        "numero":numero
+        }
+        agenda.append(contato)
+        print(f"{contato['nome']} foi adicionado")
+        time.sleep(3)
+    
+    elif opcao == 2:
+        os.system("cls")
+        if len(agenda) == 0:
+            print("lista de contatos vazia! ")
+            time.sleep(3)
+            continue 
+        for contato in agenda:
+            print("*****LISTA DE CONTATOS*****")
+                
+            print(f"Nome: {contato['nome']} --- Telefone: {contato['numero']} |")
+
+            print("*****FIM DA LISTA DE CONTATOS*****")
+            time.sleep(3)
+
+    elif opcao == 3:
+        os.system("cls")
+        print("Finalizando aplicação...")
+        time.sleep(3)
+        break
+    
+    else:
+        os.system("cls")
+        print("Opção invalida")
+        time.sleep(3)
+
+
+
+
+
+    
