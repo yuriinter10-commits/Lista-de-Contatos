af37e94038db90989ffc8b86b49caee0f79b9845
YURI DE OLIVEIRA RODRIGUES <87007240097@senacrs.edu.br>
2026-04-07 21:42:40 -0300

    agemda-de-celular M

--- agenda-de-celular2.py
+++ agenda-de-celular2.py
@@ -10,7 +10,7 @@ while True:
     print("1- Adicionar: ")
     print("2- Ver Contato: ")
     print("3- SAIR: ")
-
+    print("")
     opcao = int(input("Digite uma opcao:    "))
 
     if opcao == 1:
@@ -18,37 +18,56 @@ while True:
         email=input("Digite o e-mail:   ")
         nome=input("Digite nome:   ")
         numero=input("Digite numero:   ")
-    
-        contato = {
-        "nome": nome,
-        "numero":   numero,
-        "e-mail":   email 
-        }
-        agenda.append(contato)
-        print(f"{contato['nome']} foi adicionado")
-        time.sleep(3)
+        
+        contato_clonado = False
+
+        for contato in agenda:
+            if contato['nome'].strip().lower() == nome.strip().lower():
+                contato_clonado = True
+                break
+        if contato_clonado:
+            print(f"O contato {nome} já existe!")
+        
+        else:    
+
+            contato = {
+                "nome": nome,
+                "numero":   numero,
+                "e-mail":   email 
+            }
+            agenda.append(contato)
+            print(f"{contato['nome']} foi adicionado")
+            time.sleep(3)
     
     elif opcao == 2:
         os.system("cls")
+       
         if len(agenda) == 0:
             print("lista de contatos vazia! ")
-            time.sleep(3)
+            time.sleep(10)
             continue 
-        len(agenda)
+        
+        print(f"Voce tem {len(agenda)} contatos salvos:  ")
+        print("*****LISTA DE CONTATOS*****")
+       
         for contato in agenda:
-            print("*****LISTA DE CONTATOS*****")
-                
             print(f"Nome: {contato['nome']} --- Telefone: {contato['numero']} --- E-mail: {contato['e-mail']} |")
+            
+        print("*****FIM DA LISTA DE CONTATOS*****")
+        time.sleep(3)          
 
-            print("*****FIM DA LISTA DE CONTATOS*****")
-            time.sleep(3)
 
     elif opcao == 3:
         os.system("cls")
-        print("Finalizando aplicação...")
+        print("A agenda foi limpa!  ")
         time.sleep(3)
         break
-    
+    elif opcao == 0:
+        os.system("cls")
+        print("Finalizando aplicação...")
+        time.sleep(TEMPO)
+        break
+   
     else:
         os.system("cls")
         print("Opção invalida")
